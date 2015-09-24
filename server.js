9b1a64d41129c7ae5b2e2502df8c7db91d008171
David Watkins <djrival7@gmail.com>
2015-09-24 02:01:44 -0400

    More stuff

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
+        process.env.OPENSHIFT_APP_NAME +
+        "?authSource=admin";
 }
 var db = monk(connection_string);
 
